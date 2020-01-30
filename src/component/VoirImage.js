6031e91976ecf29d002e05221c8d03ddb108b29d
Christ-arch <dera.laroche@gmail.com>
2020-01-30 18:11:32 +0100

    ajout de méthode

--- src/component/VoirImage.js
+++ src/component/VoirImage.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 
 class VoirImage extends Component{
+  ajoutImage(i){
+    return < Ajout value={i}/>
+  }
+  suppImage(i){
+    return < Supp value={i}/>
+  }
   render(){
     return (
       <div className="VoirImage">
