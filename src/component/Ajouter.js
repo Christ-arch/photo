964b0a2168b3208c2f3641e88b9585b0de4e0e7f
Christ-arch <dera.laroche@gmail.com>
2020-02-11 12:34:03 +0100

    Ajout bouton ajouter

---
+++ src/component/Ajouter.js
@@ -0,0 +1,15 @@
+class Ajouter extends Component{
+    render(){
+      return (
+        <div className="Ajout">
+            <div>
+            AJOUTER
+            </div>
+          
+        </div>
+      );
+    }
+    
+  }
+  
+  export default Ajouter;
