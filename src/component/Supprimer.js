f14a862d2d192e979ac53c9b96e7a523f14f740b
Christ-arch <dera.laroche@gmail.com>
2020-02-11 12:29:21 +0100

    ajout bouton supprimer

---
+++ src/component/Supprimer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class Supprimer extends Component{
+  render(){
+    return (
+      <div className="Supp">
+          <div>
+          SUPPRIMER
+          </div>
+        
+      </div>
+    );
+  }
+  
+}
+
+export default Supprimer;
